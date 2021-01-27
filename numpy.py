9e64944f81111234f8771d97587cd12c09dbaf3b
stargazersham <77924363+stargazersham@users.noreply.github.com>
2021-01-28 02:05:24 +0530

    Delete numpy.py

--- numpy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-a = np.random.normal(size=(20, 20))
-b = np.random.randint(100,size=(20, 1))
-def theta(X,y):
-  xtxinv=np.linalg.inv(np.dot(X.T,X))
-  xty=np.dot(X.T,y)
-  return np.dot(xtxinv,xty)
-theta(a,b)
